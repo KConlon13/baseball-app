6cb40afd72e67792fb5502c55d6eff078d87271c
KConlon13 <KyleConlon13@gmail.com>
2020-06-02 21:34:27 -0400

    Began working with new API and successfully fetched an individual player's info and displayed it onto the page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state={
+    mainContainer: [],
+  }
+  
+  componentDidMount(){
+    fetch("http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='cespedes%25'")
+    .then(response=>response.json())
+    .then(data => this.setState({mainContainer: data.search_player_all.queryResults.row}, console.log(data.search_player_all.queryResults.row))) 
+  }
+  // Testing here to see how the API endpoints work and how I will be able to fetch individual players
+  render(){
+    return (
+      <div className="App">
+        <h1>{this.state.mainContainer.name_display_first_last}</h1>
+        <h3>{this.state.mainContainer.position} - {this.state.mainContainer.team_full}</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
