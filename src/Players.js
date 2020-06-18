525b8d9d4df2c082195665ab62d08a0a03554855
KConlon13 <KyleConlon13@gmail.com>
2020-06-18 12:08:51 -0400

    reworked search feature but still working on last bug fix

--- src/Players.js
+++ src/Players.js
@@ -1,27 +1,45 @@
 import React from 'react';
 import './App.css';
-import { Search } from 'semantic-ui-react'
-import _ from 'lodash'; 
+import { Input, Icon } from 'semantic-ui-react'
+// import _ from 'lodash'; 
 
-let results
 class Players extends React.Component {
     state={
         playerData: [],
+        searchData: []
     }
 
-    handleSearchChange = (e, { value }) => {
-        let searchTerm = value
-        console.log(searchTerm)
-        if (value.length > 0 || this.state.playerData > 0){
+    handleOnChange = (e) => {
+        console.log(e.target.value)
+        this.setState({
+            searchData: e.target.value
+        })
+        this.handleSearch(e)
+    }
+
+
+    handleSearch = (e) => {
+        let searchTerm = this.state.searchData
+        // console.log(e.target)
+        if (this.state.searchData !== []){
             fetch(`http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='${searchTerm}%25'`)
             .then(response=>response.json())
-            .then(data => this.setState({playerData: data.search_player_all.queryResults.row}))
+            .then(data => this.setState({playerData: data.search_player_all.queryResults.row}, console.log("YO")))
+
         } else {
-            value = ""
+            console.log("Ah fuck")
         }
     }
 
-    
+    keyPressed = (e) => {
+        if (e.key === "Enter"){
+            this.setState({
+                searchData: e.target.value
+            })
+            this.handleSearch(e)
+        }
+    }
+
     // ORIGINAL FETCH
     // componentDidMount(){
         //     fetch(`http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='${searchTerm}%25'`)
@@ -30,34 +48,41 @@ class Players extends React.Component {
         // }
         
     render(){
-
-    if (this.state.playerData.length > 0){
+    let results
+    if (this.state.playerData !== []){
         results = this.state.playerData.map(p => {
             // console.log("this here is a player", p),
             return <div class="ui segment">
                 <h4>{p.name_display_first_last}</h4>
             </div>
         })
-    }
-
-    if (this.state.playerData !== []){
-        console.log(this.state.playerData)
+    } else {
+        return (
+            <div class="results transition">
+            <div class="message empty"><div class="header">No results found.</div></div>
+          </div>
+        )
     }
 
         return (
             <div>
                 <h1>Search Player Stats</h1>
-                <Search
+                {/* <Search
                     fluid
                     // loading={isLoading}
                     // onResultSelect={this.handleResultSelect}
-                    onSearchChange={_.debounce(this.handleSearchChange, 500, {
+                    onSearchChange={_.debounce(this.handleSearch, 500, {
                     leading: true,
                     })}
                     // results={results}
                     // value={value}
                     // {...this.props}
-                />
+                /> */}
+                  <Input
+                    onChange={(e)=>this.handleOnChange(e)}
+                    icon={<Icon name='search' inverted circular link onClick={(e)=> this.handleSearch(e)} onKeyPress={this.keyPressed}/>}
+                    placeholder='Search...'
+                  />
                 {results}
             </div>
         )
