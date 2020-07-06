5624d76d68491ae4107cac442305989b66e95082
KConlon13 <KyleConlon13@gmail.com>
2020-07-06 14:51:54 -0400

    successfully fetching transaction data from API

--- src/Injuries.js
+++ src/Injuries.js
@@ -3,6 +3,27 @@ import './App.css';
 import { Feed, Icon, Header } from 'semantic-ui-react'
 
 class Injuries extends React.Component {
+
+    // componentDidMount(){
+    //     fetch("http://mlb-data.p.rapidapi.com/fantasylookup/json/json/named.wsfb_news_injury.bam", {
+	//     method: "GET",
+	//     headers: {
+	// 	"x-rapidapi-host": "mlb-data.p.rapidapi.com",
+    //     "x-rapidapi-key": "a052480e81msh6a6d8cb3139b9a4p1d2f1fjsn7ce1e5d89cd9",
+    //     }
+    // })
+    //     .then(data => {
+    //         console.log(data);
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //     });
+    // }
+
+
+    // The above fetch not working
+    // Need to use a better API for live injury updates, original is not functioning
+
     render(){
         return(
             <div>
--- src/Transactions.js
+++ src/Transactions.js
@@ -3,6 +3,31 @@ import './App.css';
 import { Feed, Icon, Header } from 'semantic-ui-react'
 
 class Transactions extends React.Component {
+    state={
+        transactionData: [],
+    }
+
+    componentDidMount(){
+        let now = new Date();
+        let nowDate = now.toLocaleDateString().split("/")
+        if (nowDate[1].length === 1) {
+            nowDate[1] = "0"+nowDate[1]
+        } 
+        if (nowDate[0].length === 1) {
+            nowDate[0] = "0"+nowDate[0]
+        }
+        let currentDate = nowDate[2] + nowDate[0] + nowDate[1]
+        // console.log(currentDate)
+
+        // Use today's date as the end range of the fetch
+        fetch(`https://lookup-service-prod.mlb.com/json/named.transaction_all.bam?sport_code='mlb'&start_date='20200701'&end_date=${currentDate}`)
+        .then(resp=>resp.json())
+        .then(data => (
+            this.setState({transactionData: data.transaction_all.queryResults.row}), 
+            console.log(this.state.transactionData)
+        ))
+    }
+
     render(){
         return(
             <div>
